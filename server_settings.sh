e9d144b51b1ce61e249dee8caaefaf04db0bd740
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2024-10-18 20:15:13 +0300

    Update server_settings.sh

--- server_settings.sh
+++ server_settings.sh
@@ -17,6 +17,10 @@ sudo swapon /swapfile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc
 
+# Перезагрузка текущей оболочки для применения NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Это загружает nvm
+
 # Установка последней версии LTS Node.js через NVM
 nvm install --lts
 
