f8203ca3a4c1c784af647aad4f7c7aa8eec807b5
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2024-10-18 19:51:44 +0300

    Update server_settings.sh

--- server_settings.sh
+++ server_settings.sh
@@ -3,6 +3,16 @@
 # Обновление системы
 sudo apt update && sudo apt upgrade -y
 
+# Настройка Git
+git config --global user.name "dd"
+git config --global user.email "dd5892631@gmail.com"
+
+# Создание и настройка файла подкачки
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
 # Установка NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc
@@ -14,14 +24,6 @@ nvm install --lts
 npm install -g pnpm@latest
 npm install -g bun
 
-# Настройка Git
-git config --global user.name "dd"
-git config --global user.email "dd5892631@gmail.com"
 
-# Создание и настройка файла подкачки
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
 
 echo "Server settings completed!"
