48954de0a6cdf87f8ffb3b74e44158d64bc23d2c
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2024-10-26 16:04:27 +0300

    Update settings_for_new_dev_server.sh

--- settings_for_new_dev_server.sh
+++ settings_for_new_dev_server.sh
@@ -51,4 +51,12 @@ cd "$GIT_DIR_NAME"
 cp .env.example .env
 bun i
 
+sudo apt update
+sudo apt install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install -y code
+
 shutdown -r now
